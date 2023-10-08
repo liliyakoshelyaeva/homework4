749f952c3dc4551efa3d0697a27bdc04349a0fdb
Кошеляева Лилия <lilya.koshelyaeva@mail.ru>
2023-10-08 21:26:18 +0300

    изменения

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  print('Привет, мир!')
+  var l := ReadString('Введите язык:');
+  if l = 'русский' then
+    print('Привет, мир!');
+  if l = 'английский' then
+    print('Hello, world!');
+  if l = 'французский' then
+    print('Bonjour le monde!');
+  if l = 'немецкий' then
+    print('Hallo Welt!');
+  if l = 'белорусский' then
+    print('Прывiтанне, свет!');
 end.
